712ea6912d7b9c2980eb8148789be3e1f6e95213
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-08-24 14:24:17 -0300

    Update main.py

--- LCD_sensor/main.py
+++ LCD_sensor/main.py
@@ -1,26 +1,22 @@
-# -*- coding: utf-8 -*-
-__author__ = 'LiYuanhe'
 
 import time
-from machine import Pin, SoftI2C
+from machine import Pin, SoftI2C, ADC
 from lib_lcd1602_2004_with_i2c import LCD
 
 scl_pin = 5
 sda_pin = 4
 lcd = LCD(SoftI2C(scl=Pin(scl_pin), sda=Pin(sda_pin), freq=100000))
 
+lm35 = 0 #ADC (pino analogico 0) no ESP8266
+adc = ADC(0) #Cria um objeto ADC para o ADC0
+
 # 0000000000000000
 
 while True:
-    lcd.puts("I2C LCD Sucesso ")
-    time.sleep(2)
-    lcd.clear()
-    lcd.puts("Inicializando a Contagem de 0-14")
-    time.sleep(2)
-    lcd.clear()
-    for i in range(15):
-        lcd.puts(str(i))
-        time.sleep(1)
-        lcd.clear()
+    lm35_value = adc.read()
 
+    temp_celsius = (lm35/1024)*330
 
+    lcd.puts("Temperatura: {:.2f} °C".format(temp_celsius))
+    time.sleep(2)#espera 2 segundos para ler um novo valor no sensor
+    lcd.clear()
