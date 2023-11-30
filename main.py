75a9e8f080fe6a4cdae7ab47ce600f2b00df4472
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-11-29 22:02:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ print("Tentando fazer o micro python funcionar")
 from machine import Pin
 from time import sleep
 
-led = Pin(2, Pin.OUT)
+led = Pin(25, Pin.OUT)
 led.value(False)
 
 while True:
     led.value(not led.value())
-    sleep(0.1)
+    sleep(1)
