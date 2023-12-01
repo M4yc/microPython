3553b1291e2e89fe7fadc938d1fa237e5e4aee7f
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-12-01 20:26:47 -0300

    Update main.py

--- LedW2812b/main.py
+++ LedW2812b/main.py
@@ -1,26 +1,20 @@
-import neopixel
-import machine
 import time
+from neopixel import Neopixel
+pixels = Neopixel(17, 0, 23, "GRB")
 
-# Configuração dos LEDs
-pin = machine.Pin(23)  # Escolha o número do pino apropriado
-num_leds = 1           # Número de LEDs no seu conjunto
+colors = [
+  (0xb6, 0xe4, 0x30),
+  (0x42, 0xd1, 0xe0),
+]
 
-pixels = neopixel.NeoPixel(pin, num_leds)
-
-# Função para definir a cor de todos os LEDs
-def set_color(color):
-    for i in range(num_leds):
-        pixels[i] = color
-    pixels.write()
-
-# Teste de cores
-set_color((255, 0, 0))  # Vermelho
-time.sleep(1)
-set_color((0, 255, 0))  # Verde
-time.sleep(1)
-set_color((0, 0, 255))  # Azul
-time.sleep(1)
-
-# Desligar todos os LEDs
-set_color((0, 0, 0))
+pixel_index = 0
+color_index = 0
+while True:
+  pixels.set_pixel(pixel_index, colors[color_index])
+  pixels.show()
+  #print(pixel_index, colors[color_index])
+  pixel_index += 1
+  if pixel_index == 16:
+    pixel_index = 0
+    color_index = (color_index + 1) % 2
+  time.sleep(0.1)
